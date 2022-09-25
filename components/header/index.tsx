7b4b0a6ad2066bdfe905cf33e882c1d58931e4d7
Cameron Zimmerman <cameronazimmerman@gmail.com>
2022-09-25 14:12:24 -0400

    header

---
+++ components/header/index.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useRef } from "react";
+import Link from "next/link";
+import { List, LinkedinLogo, GithubLogo, X } from "phosphor-react";
+import styles from "./Header.module.css";
+import useOutsideClick from "../../hooks/useOutsideClick";
+
+const Header: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const ref = useRef(null);
+  useOutsideClick(ref, () => {
+    if (
+      ref.current &&
+      window?.getComputedStyle(ref.current, null).getPropertyValue("left") ===
+        "0px"
+    )
+      setMenuOpen(false);
+  });
+  return (
+    <div className={styles.nav}>
+      <div className={styles.heading}>
+        <Link href="/">
+          <h1>Cameron Zimmerman</h1>
+        </Link>
+        <List
+          size={48}
+          onClick={() => {
+            return setMenuOpen(true);
+          }}
+          className={styles.list}
+        />
+      </div>
+      <div className={`${styles.socials} ${styles.link}`}>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/cameron-zimmerman/"
+        >
+          <LinkedinLogo size={48} />
+        </a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/CameronZimmerman"
+        >
+          <GithubLogo size={48} />
+        </a>
+      </div>
+      <nav
+        className={menuOpen ? styles.sidebarMenuOpen : styles.sidebarMenu}
+        ref={ref}
+      >
+        <X size={24} className={styles.x} onClick={() => setMenuOpen(false)} />
+        <h1 className={styles.menuHeader}>Menu</h1>
+        <section
+          className={`${styles.sidebarLinks} ${styles.link}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <Link href="/">Landing</Link>
+          <Link href="/resume">Resume</Link>
+          <Link href="/projects">Projects</Link>
+          <Link href="/contact">Contact</Link>
+        </section>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
