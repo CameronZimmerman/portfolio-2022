e263b0f918e60bab1d0cfd010c0ef4a5dc331ec3
Cameron Zimmerman <cameronazimmerman@gmail.com>
2022-09-25 14:11:04 -0400

    setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../globals.css";
+import Layout from "../components/layouts";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function Portfolio({ Component, pageProps }: AppProps) {
+  console.log(Component.name);
+  return Component.name !== "Resume" ? (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  ) : (
+    <Component {...pageProps} />
+  );
 }
 
-export default MyApp
+export default Portfolio;
